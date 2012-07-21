b7c8d3977dc2f047fdf18513f1f1aaea37bde1f1
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-07-21 03:01:09 -0300

    solved some bugs

--- lib/auto_html/filters/redcarpet.rb
+++ lib/auto_html/filters/redcarpet.rb
@@ -1,7 +1,7 @@
 require 'redcarpet'
 
 AutoHtml.add_filter(:redcarpet).with({}) do |text, options|
-  result = Redcarpet.new(text).to_html
+  result = RedcarpetCompat.new(text).to_html
   if options and options[:target] and options[:target].to_sym == :_blank
     result.gsub!(/<a/,'<a target="_blank"')
   end
